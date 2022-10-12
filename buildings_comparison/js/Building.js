431b173087c0a1ae48f3271498711aa6e936ac5f
diegovargasg <diegodevelopero@gmail.com>
2022-10-12 15:22:53 +0200

    added suppor sphere

--- buildings_comparison/js/Building.js
+++ buildings_comparison/js/Building.js
@@ -22,11 +22,6 @@ class Building {
       shape.holes.push(gap);
     });
 
-    // var rotation = new THREE.Matrix4().makeRotationZ(Math.PI/2);
-    // var translation = new THREE.Matrix4().makeTranslation(0, 0, 10);
-    // var transformation = rotation.multiply(translation);
-    // shape.applyMatrix(transformation);
-
     geometry = generateGeometry(shape, {
       curveSegments: 1,
       //depth: 0.05 * this.height,
@@ -36,7 +31,6 @@ class Building {
 
     geometry.rotateX(Math.PI / 2);
     geometry.rotateZ(Math.PI);
-    //geometry.translate( 0, 2, 0 );
 
     return geometry;
   }
