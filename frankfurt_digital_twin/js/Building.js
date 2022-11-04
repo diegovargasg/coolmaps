b96fb707eb274cf63aae4f64f8463a15978924ef
diegovargasg <diegodevelopero@gmail.com>
2022-11-04 14:14:10 +0100

    Refactored type of geometry

--- frankfurt_digital_twin/js/Building.js
+++ frankfurt_digital_twin/js/Building.js
@@ -1,12 +1,24 @@
 class Building {
-  constructor(coordinates, properties, height, center) {
+  constructor(coordinates, properties, center) {
     this.coordinates = coordinates;
     this.properties = properties;
-    this.height = height ? height : 1;
     this.center = center;
+
+    this.height = this.properties["height"] ? this.properties["height"] : 1;
+    this.minHeight = this.properties["minHeight"] ? this.properties["minHeight"] : 0;
+
+    this.height = this.height - this.minHeight;
   }
 
   generate() {
+    if(this.properties["shape"] === "sphere") {
+      return this.generateSphere(); 
+    } else {
+      return this.generateBox();
+    }
+  }
+
+  generateBox() {
     let shape, geometry;
     let gaps = [];
 
@@ -31,6 +43,26 @@ class Building {
 
     geometry.rotateX(Math.PI / 2);
     geometry.rotateZ(Math.PI);
+
+    if(this.minHeight > 0) {
+      geometry.translate(0, (0.01*this.minHeight), 0);
+    }
+
+    return geometry;
+  }
+
+  generateSphere() {
+    const features = turf.points(...this.coordinates);
+    const centerSphere = turf.center(features);
+    const height = this.properties["height"]
+    const minHeight = this.properties["minHeight"] ? this.properties["minHeight"] : 0;
+    const finalHeight = height - minHeight;
+    const radius = 0.01*(finalHeight/2);
+    const z = (0.01*(minHeight+finalHeight/2));
+
+    let elPosition = GPSRelativePositionTurf(centerSphere.geometry.coordinates, this.center);
+    let geometry = new THREE.SphereGeometry(radius, 16, 8);
+    geometry.translate(-elPosition[0], z, elPosition[1]);
 
     return geometry;
   }
