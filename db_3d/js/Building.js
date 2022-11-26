5fff29706517883091e1372bab7f8f3d7ffd513b
diegovargasg <diegodevelopero@gmail.com>
2022-11-26 20:58:21 +0100

    Adjusted DB logo changes

--- db_3d/js/Building.js
+++ db_3d/js/Building.js
@@ -12,9 +12,9 @@ class Building {
   }
 
   generate() {
-    if(this.properties["shape"] === "sphere") {
-      return this.generateSphere(); 
-    } else if(this.properties["roofShape"] === "pyramid") {
+    if (this.properties["shape"] === "sphere") {
+      return this.generateSphere();
+    } else if (this.properties["roofShape"] === "pyramid") {
       return this.generatePyramid();
     } else {
       return this.generateBox();
@@ -26,7 +26,7 @@ class Building {
     let gaps = [];
 
     this.coordinates.forEach((coordinate, index) => {
-      if ((index+1) > this.gaps) {
+      if (index + 1 > this.gaps) {
         shape = generateShape(coordinate, this.center);
       } else {
         gaps.push(generateShape(coordinate, this.center));
@@ -45,10 +45,8 @@ class Building {
     });
 
     geometry.rotateZ(33);
-
-    if(this.minHeight > 0) {
-      geometry.translate(0, (0.01*this.minHeight), 0);
-    }
+    geometry.translate(-5, -2.5, 0);
+    geometry.scale(10, 10, 10);
 
     return geometry;
   }
@@ -56,11 +54,11 @@ class Building {
   generateSphere() {
     const features = turf.points(...this.coordinates);
     const centerSphere = turf.center(features);
-    const height = this.properties["height"]
+    const height = this.properties["height"];
     const minHeight = this.properties["minHeight"] ? this.properties["minHeight"] : 0;
     const finalHeight = height - minHeight;
-    const radius = 0.01*(finalHeight/2);
-    const z = (0.01*(minHeight+finalHeight/2));
+    const radius = 0.01 * (finalHeight / 2);
+    const z = 0.01 * (minHeight + finalHeight / 2);
 
     let elPosition = GPSRelativePositionTurf(centerSphere.geometry.coordinates, this.center);
     let geometry = new THREE.SphereGeometry(radius, 16, 8);
@@ -74,7 +72,7 @@ class Building {
     const centroid = turf.centroid(polygon);
     const from = turf.point(centroid.geometry.coordinates);
     const to = turf.point(this.coordinates[0][0]);
-    const options = {units: 'meters'};
+    const options = { units: "meters" };
     const distance = turf.distance(from, to, options);
     const radius = 0.01 * distance;
     const features = turf.points(...this.coordinates);
@@ -82,17 +80,17 @@ class Building {
     const height = this.properties.height;
 
     let minHeight = 0;
-    if(this.properties['minHeight']) {
+    if (this.properties["minHeight"]) {
       minHeight = this.properties.minHeight * 0.01;
     }
 
-    const z = 0.01*54.4;
+    const z = 0.01 * 54.4;
     const roofHeight = this.properties.roofHeight;
 
     let elPosition = GPSRelativePositionTurf(featuresCenter.geometry.coordinates, this.center);
-    let geometry = new THREE.CylinderGeometry(0, radius, height*0.01, 4);
+    let geometry = new THREE.CylinderGeometry(0, radius, height * 0.01, 4);
     geometry.translate(-elPosition[0], z, elPosition[1]);
-  
+
     return geometry;
   }
 }
